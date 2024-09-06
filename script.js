fad6d51f30c385d96707cd8898355d61ad853799
Alek <kralj.komshiluka@gmail.com>
2024-09-06 18:05:45 +0200

    Write functions for creating and deleting various elements

--- script.js
+++ script.js
@@ -43,3 +43,58 @@ const wrapper = document.querySelector('.wrapper');
    wrapper.style.gridTemplateRows = "repeat(auto, min-content)";
    wrapper.style.alignItems = "start";
    wrapper.style.gap = "10px";
+
+   /**** Functions for creating various elements ****/
+
+function createCard() {
+  const card = document.createElement('div');
+  card.style.marginBottom = "10px";
+  card.style.padding = "10px";
+  card.style.backgroundColor = "deepskyblue";
+  card.style.border = "8px solid whitesmoke";
+  card.style.borderRadius = "20px";
+  return card;
+}
+
+function createKeyValuePairParagraph(keyValuePair) {
+  const p = document.createElement('p');
+  p.innerText = keyValuePair;
+  p.style.fontWeight = "bold";
+  p.style.textDecoration = "underline red 3px";
+  p.style.textUnderlineOffset = "5px";
+  return p;
+}
+
+function createButton(buttonText) {
+  const button = document.createElement('button');
+  button.setAttribute('type', 'button');
+  button.classList.add('deleteButton');
+  button.innerText = buttonText;
+  return button;
+}
+
+ function createCheckbox() {
+   const checkboxDivWrapper = document.createElement('div');
+  const bookReadCheckLabel = document.createElement('label');
+  bookReadCheckLabel.innerText = "READ";
+  const bookReadCheck = document.createElement('input');
+  bookReadCheck.setAttribute("type", "checkbox");
+  bookReadCheck.setAttribute("name", "is-book-read");
+  bookReadCheck.setAttribute("value", "not read");
+  bookReadCheck.style.accentColor = "red";
+  checkboxDivWrapper.append(bookReadCheckLabel, bookReadCheck);
+  return checkboxDivWrapper;
+  }
+
+// Functions for removing elements
+
+function removeCard(index, array, element) {
+ array.splice(index, 1);
+ element.remove();
+}
+
+function removeChildren(parent) {
+  while (parent.hasChildNodes()) {
+    parent.removeChild(parent.lastChild);
+  }
+}
