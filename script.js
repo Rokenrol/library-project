56b1b0d9b01d0a1656d9e7e1fa31f404e707deb3
Alek <kralj.komshiluka@gmail.com>
2024-09-06 18:14:26 +0200

    Write function to create dialog box and a form with inputs and buttons

--- script.js
+++ script.js
@@ -98,3 +98,101 @@ function removeChildren(parent) {
     parent.removeChild(parent.lastChild);
   }
 }
+
+/**** Create a dialog box and a form with inputs and buttons ****/
+
+function createForm() {
+  const dialog = document.createElement("dialog");
+  const form = document.createElement("form");
+  form.setAttribute("method", "#");
+  form.setAttribute("action", "#");
+  
+  // Title label and input
+  
+  const titleLabel = document.createElement("label");
+  titleLabel.innerText = "TITLE";
+  titleLabel.setAttribute("for", "inputTitle");
+  
+  const inputTitle = document.createElement("input");
+  inputTitle.setAttribute("id", "inputTitle");
+  inputTitle.setAttribute("type", "text");
+  inputTitle.setAttribute("name", "title");
+  inputTitle.setAttribute("placeholder", "Title");
+  
+  // Author label and input
+  
+  const authorLabel = document.createElement("label");
+  authorLabel.innerText = "AUTHOR";
+  authorLabel.setAttribute("for", "inputAuthor");
+  
+  const inputAuthor = document.createElement("input");
+  inputAuthor.setAttribute("id", "inputAuthor");
+  inputAuthor.setAttribute("type", "text");
+  inputAuthor.setAttribute("name", "author");
+  inputAuthor.setAttribute("placeholder", "Author");
+  
+  // Pages label and input
+  
+  const pagesLabel = document.createElement("label");
+  pagesLabel.innerText = "PAGES";
+  pagesLabel.setAttribute("for", "inputPages");
+  
+  const inputPages = document.createElement("input");
+  inputPages.setAttribute("id", "inputPages");
+  inputPages.setAttribute("type", "text");
+  inputPages.setAttribute("name", "pages");
+  inputPages.setAttribute("placeholder", "Pages");
+  
+  // Release date label and input
+  
+  const releaseDateLabel = document.createElement("label");
+  releaseDateLabel.innerText = "RELEASE DATE";
+  releaseDateLabel.setAttribute("for", "inputReleaseDate");
+  
+  const inputReleaseDate = document.createElement("input");
+  inputReleaseDate.setAttribute("id", "inputReleaseDate");
+  inputReleaseDate.setAttribute("type", "text");
+  inputReleaseDate.setAttribute("name", "release date");
+  inputReleaseDate.setAttribute("placeholder", "Release date");
+  
+  // Checkbox
+  
+ const bookCheckbox = createCheckbox();
+  
+  // Add button 
+  
+  const addButton = document.createElement('button');
+  addButton.setAttribute("type", "button");
+  addButton.innerText = "ADD";
+  addButton.classList.add('add-button');
+  
+  // Close button
+  
+  const closeButton = document.createElement('button');
+  closeButton.setAttribute("type", "button");
+  closeButton.innerText = "CLOSE";
+  closeButton.classList.add('close-button');
+
+  // Append all the elements to construct the form
+  
+  form.append(titleLabel, inputTitle);
+  form.append(authorLabel, inputAuthor);
+  form.append(pagesLabel, inputPages);
+  form.append(releaseDateLabel, inputReleaseDate);
+  form.append(bookCheckbox);
+  form.append(addButton, closeButton);
+  dialog.append(form);
+  document.body.append(dialog);
+  dialog.showModal();
+  
+  /*** Close modal, reset input fields and remove dialog from the DOM ****/
+  // I'm not sure if resetting the input fields is necessary because dialog
+  // gets removed entirely with dialog.remove() but I'll leave it there just in case
+  
+  const inputs = form.querySelectorAll("input");
+
+  closeButton.addEventListener('click', (event) => {
+    dialog.close();
+    inputs.forEach((input) => input.value = "");
+    dialog.remove();
+  });
